0c8c2d21da5d1aa84b54c14358233c26c7ac47e1
Timotei Dumitru <dumitru.timy@gmail.com>
2024-08-06 18:33:15 +0100

    enhance bubbleSort and added selectionSort algo

--- src/com/datastructure/sorting/SelectionSort.java
+++ src/com/datastructure/sorting/SelectionSort.java
@@ -2,31 +2,33 @@ package com.datastructure.sorting;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class SelectionSort {
     public static void main(String[] args) {
         DataGenerator dataGenerator = new DataGenerator();
         int[] array = dataGenerator.generateUniqueRandomNumbers(100);
-
         System.out.println("Array before sorting: " + Arrays.toString(array));
-        bubbleSort(array);
+        selectSort(array);
         System.out.println("Array after sorting: " + Arrays.toString(array));
-
     }
-    public static void bubbleSort(int[] array){
+
+    public static void selectSort(int[] array){
         int size = array.length;
+        int minIndex;
         int temp;
         int steps = 0;
-        for (int i=0;i<size;i++){
-            for (int j=0;j<array.length-i-1;j++){
-                if (array[j] > array[j+1]){
-                    steps++;
-                    temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+
+        for (int i=0;i<size-1;i++){
+            minIndex = i;
+            for (int j=i+1;j<size;j++){
+                if (array[minIndex] > array[j]){
+                    minIndex = j;
                 }
             }
+            temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+            steps++;
         }
         System.out.println("Number of iterations: " + steps);
     }
-
 }
