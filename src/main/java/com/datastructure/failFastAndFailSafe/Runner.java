df1de7c8041ed0bd830fdbec0330a2dd7777b3b4
Timotei Dumitru <dumitru.timy@gmail.com>
2024-08-23 20:15:28 +0100

    fail fast and fail safe with example

---
+++ src/main/java/com/datastructure/failFastAndFailSafe/Runner.java
@@ -0,0 +1,46 @@
+package com.datastructure.failFastAndFailSafe;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Runner {
+    public static void main(String[] args) {
+//        usingArrayList();
+        usingHasMap();
+    }
+
+    public static void usingArrayList(){
+        //        List<String> list = new ArrayList<>(); // fail fast
+        List<String> list = new CopyOnWriteArrayList<>(); // fail safe
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        Iterator<String> iterator = list.iterator();
+
+        while (iterator.hasNext()){
+            String elem = iterator.next(); // on fail-safe modCount will modify and will copy array before go to next iteration
+            System.out.println(elem);
+            list.add("d");
+        }
+
+        System.out.println(list);
+    }
+
+    public static void usingHasMap(){
+//        Map<Integer, String> map = new HashMap<>(); // fail-fast
+        Map<Integer, String> map = new ConcurrentHashMap<>(); // fail-safe
+        map.put(1, "Mike");
+        map.put(2, "Jane");
+
+        Iterator<String> iterator = map.values().iterator();
+
+        while (iterator.hasNext()){
+            String elem = iterator.next(); // no copy/clone concept on maps
+            System.out.println(elem);
+            map.put(3, "John");
+        }
+        System.out.println(map);
+    }
+}
