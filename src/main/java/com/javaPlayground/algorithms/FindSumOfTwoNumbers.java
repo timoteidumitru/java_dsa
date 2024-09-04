441a8457add8cf28aea4b680b437906343e02ab5
Timotei Dumitru <dumitru.timy@gmail.com>
2024-09-04 22:40:14 +0100

    find sum of two numbers with Stream Api

--- src/main/java/com/javaPlayground/algorithms/FindSumOfTwoNumbers.java
+++ src/main/java/com/javaPlayground/algorithms/FindSumOfTwoNumbers.java
@@ -1,10 +1,12 @@
 package com.javaPlayground.algorithms;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class FindSumOfTwoNumbers {
     public static void main(String[] args) {
-        findSumOfTwoNumbers(32);
+        findSumOfTwoNumbers(11);
+        findSumOfTwoNumbersWithStreams(21);
     }
 
     public static void findSumOfTwoNumbers(int target){
@@ -20,5 +22,18 @@ public class FindSumOfTwoNumbers {
                 }
             }
         }
+    }
+
+    public static void findSumOfTwoNumbersWithStreams(int target) {
+        int[] nums = {2, 3, 4, 5, 6, 7, 9, 12, 13, 14, 15, 17};
+
+        System.out.println("\nFind " + target + " by adding 2 numbers in the following array: " + Arrays.toString(nums));
+
+        // Use IntStream to loop through the indices
+        IntStream.range(0, nums.length)
+                .forEach(i -> IntStream.range(i + 1, nums.length)  // Start from i + 1 to avoid duplicates and reverse looping
+                        .filter(j -> nums[i] + nums[j] == target)  // Filter pairs where the sum equals the target
+                        .forEach(j -> System.out.println(
+                                "Numbers found: " + nums[i] + " + " + nums[j] + " = " + (nums[i] + nums[j]))));
     }
 }
