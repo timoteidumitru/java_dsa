bcd9cfcaffbe9dfae3a23a9c009b253d3b16861f
Timotei Dumitru <dumitru.timy@gmail.com>
2024-09-16 10:08:45 +0100

    multithreading using completable future

---
+++ src/main/java/com/javaPlayground/concurrency/completableFuture/UsingCompletableFuture.java
@@ -0,0 +1,40 @@
+package com.javaPlayground.concurrency.completableFuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class UsingCompletableFuture {
+    public static final Object obj = new Object();
+    private static final IntPredicate even = e -> e % 2 == 0;
+    private static final IntPredicate odd = e -> e % 2 != 0;
+
+    public static void main(String[] args) {
+        CompletableFuture.runAsync(() -> UsingCompletableFuture.printResult(odd));
+        CompletableFuture.runAsync(() -> UsingCompletableFuture.printResult(even));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void printResult(IntPredicate condition){
+        IntStream.rangeClosed(1, 10)
+                .filter(condition)
+                .forEach(UsingCompletableFuture::execute);
+    }
+
+    public static void execute(int i){
+        synchronized (obj){
+            try {
+                System.out.println("Thread name: \"" + Thread.currentThread()
+                        .getName().replaceAll(".commonPool-worker", "") + "\", with value: " + i);
+                obj.notify();
+                obj.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
