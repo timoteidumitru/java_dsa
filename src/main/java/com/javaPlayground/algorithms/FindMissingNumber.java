b2bc89f3d7fb23a72ef79330d7c9559dcf8bad90
Timotei Dumitru <dumitru.timy@gmail.com>
2024-09-18 23:33:03 +0100

    refactor find missing number from a list to java8 standard

--- src/main/java/com/javaPlayground/algorithms/FindMissingNumber.java
+++ src/main/java/com/javaPlayground/algorithms/FindMissingNumber.java
@@ -1,21 +1,22 @@
 package com.javaPlayground.algorithms;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class FindMissingNumber {
     public static void main(String[] args) {
-        int[] nums = {3,1,6,2,4,7,9};
+        List<Integer> nums = Arrays.asList(3,1,6,2,4,7,9,3);
         findMissingNumber(nums);
     }
 
-    public static void findMissingNumber(int[] nums){
+    public static void findMissingNumber(List<Integer> nums){
         int foundElem = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+        List<Integer> sortedNums = nums.stream().sorted().distinct().toList();
+        System.out.println(sortedNums);
 
-        for (int i=0;i<nums.length;i++){
-            if (nums[i+1] != nums[i] + 1) {
-                foundElem = nums[i] +1;
+        for (int i=0;i<sortedNums.size();i++){
+            if (sortedNums.get(i + 1) != sortedNums.get(i) + 1) {
+                foundElem = sortedNums.get(i) +1;
                 break;
             }
         }
